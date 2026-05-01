ea18d964238e0b4476bb2b39ef29234d5ea17a52
HiboMan <hiboman@proton.me>
2026-05-01 03:58:52 +0200

    Fix import statement for BallsDexBot

--- battle/__init__.py
+++ battle/__init__.py
@@ -2,7 +2,7 @@ import logging
 from typing import TYPE_CHECKING
 
 if TYPE_CHECKING:
-    from ballsdex.core.bot import BallsDexBot  # type: ignore
+    from ballsdex.core.bot import BallsDexBot
 
 log = logging.getLogger("ballsdex.packages.battle")
 
@@ -10,6 +10,5 @@ log = logging.getLogger("ballsdex.packages.battle")
 async def setup(bot: "BallsDexBot"):
     log.info("Loading Battle package...")
     from .cog import Battle
-
     await bot.add_cog(Battle(bot))
     log.info("Battle package loaded successfully!")
